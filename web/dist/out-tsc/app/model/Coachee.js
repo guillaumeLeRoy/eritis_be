138bc57c471558f8aa7326657f9cb6d2db6c5165
guillaume <gleroy78@gmail.com>
2017-08-07 08:37:27 +0100

    push FE dev

--- web/dist/out-tsc/app/model/Coachee.js
+++
@@ -1,8 +0,0 @@
-var Coachee = (function () {
-    function Coachee(id) {
-        this.id = id;
-    }
-    return Coachee;
-}());
-export { Coachee };
-//# sourceMappingURL=/Users/guillaume/angular/eritis_fe/src/app/model/Coachee.js.map
